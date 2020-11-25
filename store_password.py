5ce840ef91208b05c9f82cb73f5028435ffaef44
dannyvernals <50915185+dannyvernals@users.noreply.github.com>
2020-11-25 13:29:50 +0000

    Delete store_password.py

--- store_password.py
+++
@@ -1,8 +0,0 @@
-"""store juju password in local keyring"""
-import keyring
-import getpass
-
-
-username = input('username: ')
-password = getpass.getpass('password: ')
-keyring.set_password("contrail-config-diff", username, password)
